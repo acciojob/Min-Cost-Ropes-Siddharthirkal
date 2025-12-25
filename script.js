27c10319feb2cce7b76ec3e25ce9f86198dddf04
Siddharthirkal <104129670+Siddharthirkal@users.noreply.github.com>
2025-12-25 06:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function mincost(arr)
 { 
 //write your code here
+	if(arr.length<=1) return 0;
+
+	arr.sort((a,b)=>a-b);
+	let cost=0;
+
+	while(arr.length>1){
+		let first =arr.shift();
+		let second=arr.shift();
+		let sum = first+second;
+		cost+=sum;
+
+		let i=0;
+		while (i<arr.length && arr[i]<sum) i++;
+		arr.splice(i,0,sum);
+	}
+	return cost;
 // return the min cost
   
 }
